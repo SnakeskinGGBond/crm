22d555b606a0cd22e8acf97c8a6f0737564e7dfc
jxp <jixuanpu2016@outlook.com>
2021-04-29 08:56:12 +0800

    开始编写市场活动的controller

--- src/main/java/com/jbp/crm/workbench/web/controller/ActivityController.java
+++ src/main/java/com/jbp/crm/workbench/web/controller/ActivityController.java
@@ -1,13 +1,21 @@
 package com.jbp.crm.workbench.web.controller;
 
+import com.jbp.crm.exception.ActivityException;
 import com.jbp.crm.settings.domain.User;
 import com.jbp.crm.settings.service.UserService;
+import com.jbp.crm.utils.DateTimeUtil;
+import com.jbp.crm.utils.UUIDUtil;
+import com.jbp.crm.workbench.domain.Activity;
+import com.jbp.crm.workbench.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -15,6 +23,8 @@ import java.util.List;
 public class ActivityController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private ActivityService activityService;
 
     @RequestMapping("getUserList")
     @ResponseBody
@@ -22,4 +32,18 @@ public class ActivityController {
         List<User> users = userService.getUserList();
         return users;
     }
+
+    @RequestMapping("save")
+    @ResponseBody
+    public Map<String,Object> save(Activity activity, HttpServletRequest request) throws ActivityException {
+        activity.setId(UUIDUtil.getUUID());
+        activity.setCreateTime(DateTimeUtil.getSysTime());
+        activity.setCreateBy(((User)request.getSession().getAttribute("user")).getName());
+
+        activityService.save(activity);
+        Map map = new HashMap();
+        map.put("success",true);
+        return map;
+    }
+
 }
