1a57586e77a6f5777cd8dde0d09b45cd0b68e93d
jxp <jixuanpu2016@outlook.com>
2021-04-29 08:52:19 +0800

    市场活动异常类

---
+++ src/main/java/com/jbp/crm/exception/ActivityAddException.java
@@ -0,0 +1,11 @@
+package com.jbp.crm.exception;
+
+public class ActivityAddException extends ActivityException{
+    public ActivityAddException() {
+        super();
+    }
+
+    public ActivityAddException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/jbp/crm/exception/ActivityException.java
@@ -0,0 +1,11 @@
+package com.jbp.crm.exception;
+
+public class ActivityException extends Exception{
+    public ActivityException() {
+        super();
+    }
+
+    public ActivityException(String message) {
+        super(message);
+    }
+}
