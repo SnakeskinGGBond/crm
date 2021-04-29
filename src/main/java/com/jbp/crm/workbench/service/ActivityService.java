74d79dc69f3e060e30fe0b0737cb1c64b1384829
jxp <jixuanpu2016@outlook.com>
2021-04-29 08:54:03 +0800

    开始编写市场活动service代码,save相关

--- src/main/java/com/jbp/crm/workbench/service/ActivityService.java
+++ src/main/java/com/jbp/crm/workbench/service/ActivityService.java
@@ -1,4 +1,8 @@
 package com.jbp.crm.workbench.service;
 
+import com.jbp.crm.exception.ActivityException;
+import com.jbp.crm.workbench.domain.Activity;
+
 public interface ActivityService {
+    void save(Activity activity) throws ActivityException;
 }
--- src/main/java/com/jbp/crm/workbench/service/impl/ActivityServiceImpl.java
+++ src/main/java/com/jbp/crm/workbench/service/impl/ActivityServiceImpl.java
@@ -1,6 +1,9 @@
 package com.jbp.crm.workbench.service.impl;
 
+import com.jbp.crm.exception.ActivityAddException;
+import com.jbp.crm.exception.ActivityException;
 import com.jbp.crm.workbench.dao.ActivityDao;
+import com.jbp.crm.workbench.domain.Activity;
 import com.jbp.crm.workbench.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,14 @@ public class ActivityServiceImpl implements ActivityService {
     @Autowired
     private ActivityDao activityDao;
 
+    @Override
+    public void save(Activity activity) throws ActivityException {
+        if (activity.getStartDate().compareTo(activity.getEndDate()) > 0) {
+            throw new ActivityAddException("添加失败,起始时间晚于结束时间");
+        }
+        Integer res = activityDao.save(activity);
+        if (res != 1) {
+            throw new ActivityAddException("添加失败");
+        }
+    }
 }
