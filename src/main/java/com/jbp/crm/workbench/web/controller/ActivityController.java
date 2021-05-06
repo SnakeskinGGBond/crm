1b129eddcb1c77ff27dfa13f4d03260fc4e139ff
jxp <jixuanpu2016@outlook.com>
2021-05-06 08:58:57 +0800

    市场活动信息分页查询和删除的controller层编写,通过抛异常的方式反馈删除失败信息

--- src/main/java/com/jbp/crm/workbench/web/controller/ActivityController.java
+++ src/main/java/com/jbp/crm/workbench/web/controller/ActivityController.java
@@ -5,6 +5,7 @@ import com.jbp.crm.settings.domain.User;
 import com.jbp.crm.settings.service.UserService;
 import com.jbp.crm.utils.DateTimeUtil;
 import com.jbp.crm.utils.UUIDUtil;
+import com.jbp.crm.vo.PaginationVO;
 import com.jbp.crm.workbench.domain.Activity;
 import com.jbp.crm.workbench.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,20 +29,56 @@ public class ActivityController {
 
     @RequestMapping("getUserList")
     @ResponseBody
-    public List<User> getUserList(){
+    public List<User> getUserList() {
         List<User> users = userService.getUserList();
         return users;
     }
 
     @RequestMapping("save")
     @ResponseBody
-    public Map<String,Object> save(Activity activity, HttpServletRequest request) throws ActivityException {
+    public Map<String, Object> save(Activity activity, HttpServletRequest request) throws ActivityException {
         activity.setId(UUIDUtil.getUUID());
         activity.setCreateTime(DateTimeUtil.getSysTime());
-        activity.setCreateBy(((User)request.getSession().getAttribute("user")).getName());
-
+        activity.setCreateBy(((User) request.getSession().getAttribute("user")).getName());
         activityService.save(activity);
         Map map = new HashMap();
+        map.put("success", true);
+        return map;
+    }
+
+    /**
+     * 查询市场活动信息列表
+     *
+     * @param activity
+     * @param pageNo
+     * @param pageSize 每页展示的记录条数
+     * @return
+     */
+    @RequestMapping("pageList")
+    @ResponseBody
+    public PaginationVO<Activity> pageList(Activity activity, String pageNo, String pageSize) {
+        Integer pageNoInt = Integer.valueOf(pageNo);
+        Integer pageSizeInt = Integer.valueOf(pageSize);
+        //计算出略过的记录数
+        Integer skipCount = (pageNoInt - 1) * pageSizeInt;
+        PaginationVO<Activity> vo = activityService.pageList(activity, pageSizeInt, skipCount);
+        Map<String, Object> map = new HashMap<>();
+        map.put("total", vo.getTotal());
+        map.put("dataList", vo.getDataList());
+        return vo;
+    }
+
+    /**
+     * 删除市场活动信息
+     * @param id workbench/activity/index.jsp传来的选中的市场活动信息的id
+     * @return 删除成功
+     * @throws ActivityException 市场活动信息操作的相关错误
+     */
+    @RequestMapping("delete")
+    @ResponseBody
+    public Map<String,Object> delete(String[] id) throws ActivityException {
+        activityService.delete(id);
+        Map<String,Object> map = new HashMap<>();
         map.put("success",true);
         return map;
     }
