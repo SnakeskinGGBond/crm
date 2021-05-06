7778b039b73950bc2c7eab5b1ef8fde80e796ecd
jxp <jixuanpu2016@outlook.com>
2021-05-06 08:35:10 +0800

    spring统一处理市场活动删除的Exception

--- src/main/java/com/jbp/crm/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jbp/crm/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.jbp.crm.handler;
 
 import com.jbp.crm.exception.ActivityAddException;
+import com.jbp.crm.exception.ActivityDeleteException;
 import com.jbp.crm.exception.LoginException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,6 +27,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ActivityAddException.class)
     @ResponseBody
     public Map<String,Object> activityAddException(Exception e){
+        Map<String,Object> map = new HashMap<>();
+        map.put("msg",e.getMessage());
+        map.put("success",false);
+        return map;
+    }
+
+    @ExceptionHandler(ActivityDeleteException.class)
+    @ResponseBody
+    public Map<String,Object> activityDeleteException(Exception e){
         Map<String,Object> map = new HashMap<>();
         map.put("msg",e.getMessage());
         map.put("success",false);
