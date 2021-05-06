0e95b1049b3546265a169c367fd712ce09853aec
jxp <jixuanpu2016@outlook.com>
2021-05-06 08:34:29 +0800

    开始编写市场活动删除的Exception

---
+++ src/main/java/com/jbp/crm/exception/ActivityDeleteException.java
@@ -0,0 +1,11 @@
+package com.jbp.crm.exception;
+
+public class ActivityDeleteException extends ActivityException{
+    public ActivityDeleteException() {
+        super();
+    }
+
+    public ActivityDeleteException(String message) {
+        super(message);
+    }
+}
