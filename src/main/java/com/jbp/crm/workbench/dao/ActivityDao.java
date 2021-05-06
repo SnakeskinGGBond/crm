57da5a87d0aaa7aba884474040cd9bbb5e596a3f
jxp <jixuanpu2016@outlook.com>
2021-05-06 08:37:55 +0800

    开始编写市场活动查询总条数的dao层,条件查询的dao层,删除的dao层

--- src/main/java/com/jbp/crm/workbench/dao/ActivityDao.java
+++ src/main/java/com/jbp/crm/workbench/dao/ActivityDao.java
@@ -2,6 +2,15 @@ package com.jbp.crm.workbench.dao;
 
 import com.jbp.crm.workbench.domain.Activity;
 
+import java.util.List;
+import java.util.Map;
+
 public interface ActivityDao {
     Integer save(Activity activity);
+
+    Integer getTotalByCondition(Map<String, Object> map);
+
+    List<Activity> getActivityListByCondition(Map<String, Object> map);
+
+    Integer delete(String[] id);
 }
