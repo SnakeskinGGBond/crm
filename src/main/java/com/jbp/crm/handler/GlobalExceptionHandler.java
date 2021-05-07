66066564783cd3585b0decf82c0931ed867345b1
jxp <jixuanpu2016@outlook.com>
2021-05-07 15:25:57 +0800

    统一处理修改异常

--- src/main/java/com/jbp/crm/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jbp/crm/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.jbp.crm.handler;
 
 import com.jbp.crm.exception.ActivityAddException;
 import com.jbp.crm.exception.ActivityDeleteException;
+import com.jbp.crm.exception.ActivityUpdateException;
 import com.jbp.crm.exception.LoginException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -36,6 +37,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ActivityDeleteException.class)
     @ResponseBody
     public Map<String,Object> activityDeleteException(Exception e){
+        Map<String,Object> map = new HashMap<>();
+        map.put("msg",e.getMessage());
+        map.put("success",false);
+        return map;
+    }
+    @ExceptionHandler(ActivityUpdateException.class)
+    @ResponseBody
+    public Map<String,Object> activityUpdateException(Exception e){
         Map<String,Object> map = new HashMap<>();
         map.put("msg",e.getMessage());
         map.put("success",false);
