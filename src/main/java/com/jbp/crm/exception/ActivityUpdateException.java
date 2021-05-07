0d6ec3f4a5aafce540e4ef004ad108fe1dabf79f
jxp <jixuanpu2016@outlook.com>
2021-05-07 15:25:16 +0800

    市场活动修改异常

---
+++ src/main/java/com/jbp/crm/exception/ActivityUpdateException.java
@@ -0,0 +1,11 @@
+package com.jbp.crm.exception;
+
+public class ActivityUpdateException extends ActivityException{
+    public ActivityUpdateException() {
+        super();
+    }
+
+    public ActivityUpdateException(String message) {
+        super(message);
+    }
+}
