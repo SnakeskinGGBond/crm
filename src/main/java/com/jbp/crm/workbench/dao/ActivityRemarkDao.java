d45aee1945345762029727fe06b9ecbbf112ec04
jxp <jixuanpu2016@outlook.com>
2021-05-07 15:28:15 +0800

    通过id获取备注信息dao

--- src/main/java/com/jbp/crm/workbench/dao/ActivityRemarkDao.java
+++ src/main/java/com/jbp/crm/workbench/dao/ActivityRemarkDao.java
@@ -1,7 +1,13 @@
 package com.jbp.crm.workbench.dao;
 
+import com.jbp.crm.workbench.domain.ActivityRemark;
+
+import java.util.List;
+
 public interface ActivityRemarkDao {
     Integer getCountByAids(String[] id);
 
     Integer deleteByAids(String[] id);
+
+    List<ActivityRemark> getRemarkListByAid(String id);
 }
